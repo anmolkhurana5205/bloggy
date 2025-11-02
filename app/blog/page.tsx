71b2bf584049836527ece2c627cc47fd5237a54d
anmolkhurana5205 <anmol.2327cse1235@kiet.edu>
2025-11-02 20:06:05 +0530

    same as home done

---
+++ app/blog/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BlogList from "../components/BlogList";
+
+const Page: React.FC = () => {
+  return (
+    <>
+      <BlogList />
+    </>
+  );
+};
+
+export default Page;
